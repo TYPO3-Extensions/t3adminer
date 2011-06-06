5368a07381a3c8e9375a89cfff6c68b4f7658ff9
jigal <jigal@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-06-06 10:53:58 +0000

    Initial upload
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/t3adminer/trunk@48386 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,48 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "t3adminer".
+#
+# Auto generated 03-06-2011 23:06
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Adminer',
+	'description' => 'Database administration tool \'Adminer\'',
+	'category' => 'module',
+	'author' => 'Jigal van Hemert',
+	'author_email' => 'jigal@xs4all.nl',
+	'shy' => '',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => 'mod1',
+	'doNotLoadInFE' => 1,
+	'state' => 'alpha',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '0.0.1',
+	'constraints' => array(
+		'depends' => array(
+			'typo3' => '4.5.0-0.0.0',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:17:{s:9:"ChangeLog";s:4:"ff99";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"147c";s:14:"ext_tables.php";s:4:"5aff";s:14:"doc/manual.sxw";s:4:"e67c";s:19:"doc/wizard_form.dat";s:4:"9829";s:20:"doc/wizard_form.html";s:4:"2c28";s:13:"mod1/conf.php";s:4:"a4f1";s:14:"mod1/index.php";s:4:"f088";s:18:"mod1/locallang.xml";s:4:"4076";s:22:"mod1/locallang_mod.xml";s:4:"0ace";s:19:"mod1/moduleicon.gif";s:4:"147c";s:21:"res/adminer-3.2.2.php";s:4:"136c";s:15:"res/adminer.css";s:4:"d0c0";s:17:"res/t3adminer.php";s:4:"335f";s:29:"res/plugins/AdminerFrames.php";s:4:"903a";s:22:"res/plugins/plugin.php";s:4:"9970";}',
+	'suggests' => array(
+	),
+);
+
+?>
---
+++ res/t3adminer.php
@@ -0,0 +1,101 @@
+<?php
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2011 Jigal van Hemert <jigal@xs4all.nl>
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+/**
+ * [CLASS/FUNCTION INDEX of SCRIPT]
+ *
+ * Hint: use extdeveval to insert/update function index above.
+ */
+
+function adminer_object() {
+
+		// required to run any plugin
+	include_once "./plugins/plugin.php";
+
+		// autoloader
+	foreach (glob("plugins/*.php") as $filename) {
+		include_once "./$filename";
+	}
+
+	$plugins = array(
+			// specify enabled plugins here
+		new AdminerFrames
+	);
+
+	class AdminerSoftware extends AdminerPlugin {
+
+		function name() {
+			// custom name in title and heading
+			return 'T3Adminer';
+		}
+
+		function permanentLogin() {
+			// key used for permanent login
+			return "74b941992ef29727ccabf82889fe837a";
+		}
+
+		function credentials() {
+			// server, username and password for connecting to database
+			return array(
+				$_SESSION['ADM_server'], $_SESSION['ADM_user'], $_SESSION['ADM_password']
+			);
+		}
+
+		function database() {
+			// database name, will be escaped by Adminer
+			return $_SESSION['ADM_db'];
+		}
+
+		/** Prints table list in menu
+		 * @param array
+		 * @return null
+		 */
+		function tablesPrint($tables) {
+			echo '<p id="tables">' . "\n";
+			foreach ($tables as $table => $type) {
+				echo '<span class="tables-line';
+				echo ($_GET['select'] == $table || $_GET['table'] == $table) ? ' current' : '';
+				echo '">';
+				echo var_export($_GET['select'] == $table || $_GET['table'] == $table, TRUE);
+				echo '<a href="' . h(ME) . 'select=' . urlencode($table) . '"' . bold($_GET["select"] == $table) . ">" .
+					lang('select') . "</a> ";
+				echo '<a href="' . h(ME) . 'table=' . urlencode($table) . '"' . bold($_GET["table"] == $table) . ">" .
+					$this->tableName(array(
+										  "Name" => $table
+									 )) . "</a></span>\n"; //! Adminer::tableName may work with full table status
+			}
+			echo '</p>';
+		}
+
+
+	}
+
+	return new AdminerSoftware($plugins);
+}
+
+$session_name = 'tx_t3adminer';
+session_name($session_name);
+session_start();
+
+include "./adminer-3.2.2.php";
+?>
