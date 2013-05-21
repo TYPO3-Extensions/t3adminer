ff1bfef304b86496fb0f8e11bae35ea07edfa662
jigal <jigal@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-05-21 22:23:09 +0000

    #48395 TYPO3 6.2 compatibility
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/t3adminer/trunk@75464 735d13b6-9817-0410-8766-e36946ffe9aa

--- mod1/index.php
+++ mod1/index.php
@@ -29,7 +29,6 @@
 
 
 $LANG->includeLLFile('EXT:t3adminer/mod1/locallang.xml');
-require_once(PATH_t3lib . 'class.t3lib_scbase.php');
 $BE_USER->modAccess($MCONF,1);	// This checks permissions and exits if the users has no permission for entry.
 	// DEFAULT initialization of a module [END]
 
