2d27278c97bf1295f9d406dd7e3f292eaac4aaee
jigal <jigal@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-05-26 16:50:12 +0000

    Upgrade to adminer 3.7.0, compatibility with TYPO3 CMS 6.2, CSS fixes; new version 1.3.1
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/t3adminer/trunk@75533 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "t3adminer".
  *
- * Auto generated 30-01-2013 01:42
+ * Auto generated 26-05-2013 18:24
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'Database administration tool \'Adminer\'',
 	'category' => 'module',
 	'author' => 'Jigal van Hemert',
-	'author_email' => 'jigal@xs4all.nl',
+	'author_email' => 'jigal.van.hemert@typo3.org',
 	'shy' => '',
 	'dependencies' => '',
 	'conflicts' => '',
@@ -29,17 +29,17 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '1.2.0',
+	'version' => '1.3.1',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '4.5.0-6.0.99',
+			'typo3' => '4.5.0-6.1.99',
 		),
 		'conflicts' => array(
 		),
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:21:{s:9:"ChangeLog";s:4:"37fe";s:21:"ext_conf_template.txt";s:4:"4f78";s:12:"ext_icon.gif";s:4:"147c";s:17:"ext_localconf.php";s:4:"394d";s:14:"ext_tables.php";s:4:"5aff";s:10:"README.txt";s:4:"1380";s:36:"classes/class.tx_t3adminer_hooks.php";s:4:"6b46";s:14:"doc/manual.sxw";s:4:"1a8c";s:19:"doc/wizard_form.dat";s:4:"9829";s:20:"doc/wizard_form.html";s:4:"2c28";s:13:"mod1/conf.php";s:4:"a4f1";s:14:"mod1/index.php";s:4:"5cf8";s:18:"mod1/locallang.xml";s:4:"48e5";s:22:"mod1/locallang_mod.xml";s:4:"0ace";s:19:"mod1/moduleicon.gif";s:4:"147c";s:15:"res/adminer.css";s:4:"d947";s:15:"res/adminer.txt";s:4:"453f";s:19:"res/adminer_old.txt";s:4:"3abf";s:17:"res/t3adminer.php";s:4:"c03b";s:29:"res/plugins/AdminerFrames.php";s:4:"c609";s:22:"res/plugins/plugin.php";s:4:"9970";}',
+	'_md5_values_when_last_written' => 'a:20:{s:9:"ChangeLog";s:4:"f908";s:21:"ext_conf_template.txt";s:4:"4f78";s:12:"ext_icon.gif";s:4:"147c";s:17:"ext_localconf.php";s:4:"5810";s:14:"ext_tables.php";s:4:"5aff";s:10:"README.txt";s:4:"1380";s:36:"classes/class.tx_t3adminer_hooks.php";s:4:"6b46";s:14:"doc/manual.sxw";s:4:"1a8c";s:19:"doc/wizard_form.dat";s:4:"9829";s:20:"doc/wizard_form.html";s:4:"2c28";s:13:"mod1/conf.php";s:4:"a4f1";s:14:"mod1/index.php";s:4:"23a7";s:18:"mod1/locallang.xml";s:4:"48e5";s:22:"mod1/locallang_mod.xml";s:4:"0ace";s:19:"mod1/moduleicon.gif";s:4:"147c";s:15:"res/adminer.css";s:4:"c6d0";s:15:"res/adminer.txt";s:4:"3007";s:17:"res/t3adminer.php";s:4:"c03b";s:29:"res/plugins/AdminerFrames.php";s:4:"c609";s:22:"res/plugins/plugin.php";s:4:"9970";}',
 	'suggests' => array(
 	),
 );
